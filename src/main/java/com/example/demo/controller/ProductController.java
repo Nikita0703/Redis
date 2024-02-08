c936317c10395f4ed7e7db807e0d7815bcae50a1
Nikita0703 <nikita.noviczenko2005@gmail.com>
2024-02-08 18:19:46 +0300

    Spring Boot+Redis

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+@AllArgsConstructor
+public class ProductController {
+
+    private final ProductRepository dao;
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return dao.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return dao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findProduct(@PathVariable int id) {
+        return dao.findProductById(id);
+    }
+    @DeleteMapping("/{id}")
+    public String remove(@PathVariable int id)   {
+        return dao.deleteProduct(id);
+    }
+
+}
