9ae6c83b68c464249b2c389a4f20fe105d015d6b
Nikita0703 <nikita.noviczenko2005@gmail.com>
2024-02-08 18:36:22 +0300

    Cache

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -3,12 +3,16 @@ package com.example.demo.controller;
 import com.example.demo.entity.Product;
 import com.example.demo.repository.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/product")
+@EnableCaching
 @AllArgsConstructor
 public class ProductController {
 
@@ -25,10 +29,13 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
+    @Cacheable(key = "#id",value = "Product",unless = "#result.price>1000")
     public Product findProduct(@PathVariable int id) {
         return dao.findProductById(id);
     }
+
     @DeleteMapping("/{id}")
+    @CacheEvict(key = "#id",value = "Product")
     public String remove(@PathVariable int id)   {
         return dao.deleteProduct(id);
     }
